fffbd4915b73cc89964db69df38eff1d1fbefa47
Eric <ericlg.link@gmail.com>
2022-11-17 20:09:54 -0800

    Set up the index route

--- backend/controllers/cardsRoutes.js
+++ backend/controllers/cardsRoutes.js
@@ -1,7 +1,12 @@
 // DEPENDENCIES
 require('dotenv').config()
+const { Router } = require('express')
 const express = require('express')
 const cards = express.Router()
 
-//
+// INDEX
+cards.get('/', (req, res) => {
+    res.json({mssg: 'GET all cards'})
+})
+
 
