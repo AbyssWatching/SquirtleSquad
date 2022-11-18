084f1a323d1e6571d5424937a835b5f3af26193e
Eric <ericlg.link@gmail.com>
2022-11-17 20:12:25 -0800

    Set up POST and DELETE routes

--- backend/controllers/cardsRoutes.js
+++ backend/controllers/cardsRoutes.js
@@ -9,4 +9,13 @@ cards.get('/', (req, res) => {
     res.json({mssg: 'GET all cards'})
 })
 
+// POST, ideally this the route that gets used when the "Gatcha" is rolled
+cards.post('/', (req, res) => {
+    res.json({mssg: 'POST a new card'})
+})
+
+// DELETE a card from the collection
+cards.post('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a card'})
+})
 
