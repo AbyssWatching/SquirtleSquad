b72033294d2b2b25e6af5b6f8fac284ef28634a1
Laith Albusairi <laithalbusairi@gmail.com>
2022-11-18 03:11:55 -0800

    finsied navbar start of made homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-
 import Navbar from './components/Navbar';
-
+import Home from './components/Home';
 
 
 
 function App() {
   const title = 'Welcome to Poke-Cha'
   
-  
   return (
     <div className="App">
+       <Navbar />
       <h1>{ title }</h1>
 
 
