10dd06d934954f7526cbbb276c0809f1886aafe3
Jerrorld <jerrorldk@gmail.com>
2022-11-20 10:52:42 -0800

    started on Collections and Pokemon cards components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Collection from './components/Collection';
 
 function App() {
   return (
     <div className="App">
-      
+      <Collection />
     </div>
   );
 }
---
+++ frontend/src/components/Collection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PokeCard from './PokeCard'
+
+export default function Collection() {
+  return (
+    <div className='collectionpage'>Collection
+        <PokeCard />
+    </div>
+    
+  )
+}
