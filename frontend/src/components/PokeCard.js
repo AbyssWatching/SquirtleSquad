b27f9bb5a50694bb961620a7254a4924d24bae82
Jerrorld <jerrorldk@gmail.com>
2022-11-20 11:31:51 -0800

    created css for pokecard

--- frontend/src/components/PokeCard.js
+++ frontend/src/components/PokeCard.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import '../assets/css/pokecard.css'
 
 export default function PokeCard() {
   return (
-    <div>PokeCard</div>
+    <div className='pokecard'>PokeCard</div>
   )
 }
