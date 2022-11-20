e307119e44e41963a1baacf4182ca603bd9b0b85
Eric <ericlg.link@gmail.com>
2022-11-20 05:40:08 -0800

    Random pokemon can be fetched, needs more testing

---
+++ backend/assets/gacha.js
@@ -0,0 +1,15 @@
+function rollGacha(e)
+
+// Use this as a reference to build this system off of.
+// Excluding the new Scarlet and Violet game there 905 Pokemon available.
+function randomPokemon() {
+    var randomNumber = Math.floor(Math.random() * 905) + 1;
+    var url = 'http://pokeapi.co/api/v2/pokemon/' + randomNumber;
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      });
+  }
