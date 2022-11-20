9b9eb9aba3d222f042970e75c370d7566d6374c4
Eric <ericlg.link@gmail.com>
2022-11-20 05:52:19 -0800

    Trying to leave more comments

--- backend/assets/gacha.js
+++ backend/assets/gacha.js
@@ -16,32 +16,33 @@ function randomPokemon() {
 
 randomPokemon() //TEST
 
-function postPokemon() {
-    var randomNumber = Math.floor(Math.random() * 1000) + 1;
-    var url = 'http://pokeapi.co/api/v2/pokemon/' + randomNumber;
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        var pokemon = {
-          name: data.name,
-          image: data.sprites.front_default,
-          type: data.types[0].type.name
-        };
-        var url = 'http://localhost:3000/pokemon';
-        fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(pokemon)
-        })
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(data) {
-            console.log(data);
-          });
-      });
-  }
+//This should be a more complete way to roll the gacha but it needs testing
+// function postPokemon() {
+//     var randomNumber = Math.floor(Math.random() * 1000) + 1;
+//     var url = 'http://pokeapi.co/api/v2/pokemon/' + randomNumber;
+//     fetch(url)
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(data) {
+//         var pokemon = {
+//           name: data.name,
+//           image: data.sprites.front_default,
+//           type: data.types[0].type.name
+//         };
+//         var url = 'http://localhost:3000/pokemon';
+//         fetch(url, {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify(pokemon)
+//         })
+//           .then(function(response) {
+//             return response.json();
+//           })
+//           .then(function(data) {
+//             console.log(data);
+//           });
+//       });
+//   }
