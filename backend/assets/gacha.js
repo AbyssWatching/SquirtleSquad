15091748a4b1c1c797d76eb9e67cdb331894d72a
Eric <ericlg.link@gmail.com>
2022-11-20 05:50:20 -0800

    Simple pokemon model built

--- backend/assets/gacha.js
+++ backend/assets/gacha.js
@@ -15,3 +15,33 @@ function randomPokemon() {
   }
 
 randomPokemon() //TEST
+
+function postPokemon() {
+    var randomNumber = Math.floor(Math.random() * 1000) + 1;
+    var url = 'http://pokeapi.co/api/v2/pokemon/' + randomNumber;
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var pokemon = {
+          name: data.name,
+          image: data.sprites.front_default,
+          type: data.types[0].type.name
+        };
+        var url = 'http://localhost:3000/pokemon';
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(pokemon)
+        })
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            console.log(data);
+          });
+      });
+  }
