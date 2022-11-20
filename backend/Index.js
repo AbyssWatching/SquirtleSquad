367bd16a837bdef23d415652a36c32e7409b5ae3
Winston Grant <winstonf.grant@hotmail.com>
2022-11-20 12:24:48 -0800

    DB is connectedgit add .

--- backend/Index.js
+++ backend/Index.js
@@ -1,11 +1,27 @@
 
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const express = require('express');
+const moongoose = require ('mongoose');
+const cors = require('cors');
 
 //CONFIG
+const app = express()
 require('dotenv').config()
 app.use(express.json())
+
+app.use(cors());
+app.use(express.json())
+
+//BD stuff connecting uh I see the light!
+const uri = process.env.ATLAS_URI;
+moongoose.connect(uri);
+
+
+const connection = moongoose.connection;
+connection.once('open', ()=> {
+    console.log(`YOU SEE THIS THE DB Connected suck a D***`);
+})
+
 
 const port = process.env.PORT;
 
