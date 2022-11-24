85e5cbcfce97150cd7e14d9bb060b6c7d6504d30
Eric <ericlg.link@gmail.com>
2022-11-24 05:23:17 -0800

    Connected frontend to backend root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:9000")
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message))
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <p> {message} </p>
     </div>
-  );
+  )
+
 }
 
 export default App;
