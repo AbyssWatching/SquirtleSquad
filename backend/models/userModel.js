909c6944e2f32c090bf55133f5e56f2a74d8a5ae
Eric <ericlg.link@gmail.com>
2022-11-25 06:10:40 -0800

    Signup with validation built

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,6 +18,32 @@ const userSchema = new Schema({
 
 
 // sign up method with validation
+userSchema.statics.signup = async function(email, password) {
+
+    // VALIDATE
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
 
 //login method
 
