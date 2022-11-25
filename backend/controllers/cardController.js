4bfc79e1b6f12faaa0f2660a8f982ae3aac00f21
Eric <ericlg.link@gmail.com>
2022-11-25 08:11:03 -0800

    Get all cards in collection route established

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -1,2 +1,12 @@
 const Card = require('../models/cardModel')
 const mongoose = require('mongoose')
+
+// GET ALL CARDS IN COLLECTION
+const getCards = async (req, res) => {
+    const user_id = req.user._id
+  
+    const cards = await Card.find({user_id}).sort({createdAt: -1})
+  
+    res.status(200).json(cards)
+  }
+//
