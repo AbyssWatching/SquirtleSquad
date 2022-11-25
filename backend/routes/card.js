f3df08256016c91eec71b29c23d7b289c2fdbd81
Eric <ericlg.link@gmail.com>
2022-11-25 08:30:13 -0800

    Figure out post route

--- backend/routes/card.js
+++ backend/routes/card.js
@@ -5,4 +5,8 @@ const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
 
+// IN PLACE TO REQUIRE AUTH FOR ALL ROUTES
 router.use(requireAuth)
+
+// GET ALL CARDS IN COLLECTION
+router.get('/', getCards)
