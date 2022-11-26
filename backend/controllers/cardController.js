2a5f3e8e2ac22545004b5144988555aa9c697147
Eric <ericlg.link@gmail.com>
2022-11-25 20:55:33 -0800

    Got axios, trying to post a card to db

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -9,6 +9,18 @@ const getCards = async (req, res) => {
   
     res.status(200).json(cards)
   }
-//
+// ADD NEW CARD TO COLLECTION
+const postCard = async (req, res) => {
+    const {id, name, type1, type2, weight, height, image} = req.pokemonDataToPost
+
+    try {
+        const user_id = req.user._id
+        const card = await Card.create({id, name, type1, type2, weight, height, image})
+        res.status(200).json(card)
+    } catch (err) {
+        res.status(400).json({err: error.message})
+    }
+}
+
 
 module.exports = {getCards}
--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -10,3 +10,7 @@ router.use(requireAuth)
 
 // GET ALL CARDS IN COLLECTION
 router.get('/', getCards)
+
+router.post('/')
+
+module.exports = router
