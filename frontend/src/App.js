4f60b8a64289d2f31f10780c3100d971f6998aba
Jerrorld <jerrorldk@gmail.com>
2022-11-25 22:22:08 -0800

    added button in gacha page:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import Collection from './components/Collection';
+import Gacha from './components/Gacha';
+// import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Gacha />
       <Collection />
     </div>
   );
---
+++ frontend/src/components/Gacha.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Gacha() {
+  return (
+    <div>Gacha page
+        <button className='gachaButton' onClick={clickMe}> Play </button>
+    </div>
+  )
+}
