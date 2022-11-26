388b4bebd5c017bed56d68843cddf4f1e0aa7f5d
Eric <ericlg.link@gmail.com>
2022-11-26 04:30:32 -0800

    ref dispatch

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,2 +1,15 @@
 import { useAuthContext } from "./useAuthContext";
 import { useCardsContext } from "./useCardsContext";
+
+export const useLogout = () => {
+    const { dispatch } = useAuthContext()
+    
+
+    const logout = () => {
+        // REMOVE USER FROM STORAGE
+        localStorage.removeItem('user')
+
+        // DISPATCH LOGOUT
+        dispatch({type: 'LOGOUT'})
+    }
+}
