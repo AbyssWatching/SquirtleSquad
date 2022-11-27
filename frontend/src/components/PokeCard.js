26cbce5ae1c7865b60851e46c469fdd4364f149e
Jerrorld <jerrorldk@gmail.com>
2022-11-26 19:26:15 -0800

    updated skeleton of pokemon card and css

--- frontend/src/components/PokeCard.js
+++ frontend/src/components/PokeCard.js
@@ -3,8 +3,20 @@ import '../assets/css/pokecard.css'
 
 export default function PokeCard() {
   return (
-    <div className='pokecard'>PokeCard</div>
+    <div className='pokecard'>
+      <div className='pokecard-container'>
+        <div className='pokecard-image'>
+          <image className='sprite' alt='pokemon sprite'>image</image>
+        </div>
+        <div className='pokecard-content-container'>
+          <h1 className='pokemon-name'>Name</h1>
+          <p className='pokemon-height'>Height:</p>
+          <p className='pokemon-weight'>Weight:</p>
+        </div>
+      </div>
+    </div>
     // design card with css
     // fetch data from pokeapi and put on card
+     
   )
 }
