83f89fe06fd6be09ff7294c551d5dc889ae6f115
Laith Albusairi <laithalbusairi@gmail.com>
2022-11-29 04:19:19 -0800

    added divs

--- frontend/src/views/Signup.js
+++ frontend/src/views/Signup.js
@@ -16,25 +16,31 @@ const Signup = () => {
   }
 
   return (
-    <form className="signup" onSubmit={handleSubmit}>
-      <h3>Sign Up</h3>
-      
-      <label>Email address:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
-      />
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
-      />
 
-      <button disabled={isLoading}>Sign up</button>
-      {error && <div className="error">{error}</div>}
-    </form>
+    <div className="main">
+      <div className="main1">
+    
+          <form className="signup" onSubmit={handleSubmit}>
+          <h3>Sign Up</h3>
+      
+          <label>Email address:</label>
+          <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email} 
+          />
+          <label>Password:</label>
+          <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+          />
+
+         <button disabled={isLoading}>Sign up</button>
+         {error && <div className="error">{error}</div>}
+        </form>
+      </div>
+    </div>
   )
 }
 
