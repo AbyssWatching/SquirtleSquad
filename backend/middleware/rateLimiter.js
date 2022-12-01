5a5158b026bdb2cc38ec0fb82c6f288a36dc42f1
Eric <ericlg.link@gmail.com>
2022-11-30 19:57:31 -0800

    Update the error message but we need to clear it preferably

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimiter = require("express-rate-limit");
 const limiter = rateLimiter({
 max: 3,
 windowMS: 86000000, //24 Hours
-message: "You're out of pokeballs, come back tomorrow",
+message: 'no more pokeballs come back in 24 hours',
 });
 
 module.exports = limiter
